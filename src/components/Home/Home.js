d73a483f324c8c69041a990ad9ec0e06465606d8
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-05-31 21:36:21 +0600

    fixing home page details

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
     const [review, setReview] = useReview();
     return (
         <div className="container">
-            <div className="row m-6">
-                <div className="col-md-8 p-6 border-2">
+            <div className="row m-6 pb-3">
+                <div className="col-md-8 p-6 ">
                     <h1 className="text-justify mb-6" >Welcome To <br /> <span className="text-amber-900 ml-5">Boots Shop</span></h1>
-                    <h6 className='text-justify p-1'>We are one of the only places you will find that still measures your feet to help ensure you get that proper fit.  We will help you find shoes to accomodate width requirements, orthotics, or braces.  The Boot Shop is a full service shoe store with a large selection of fine family footwear.  From dress to casual, athletic to slippers and safety, we have it all including accessories like insoles, polishes, laces and so much more.</h6>
+                    <p className='text-justify p-1'>We are one of the only places you will find that still measures your feet to help ensure you get that proper fit.  We will help you find shoes to accomodate width requirements, orthotics, or braces.  The Boot Shop is a full service shoe store with a large selection of fine family footwear.  From dress to casual, athletic to slippers and safety, we have it all including accessories like insoles, polishes, laces and so much more.</p>
                 </div>
                 <div className=" col-md-4 p-3 ">
                     <img className='ml-1' src="https://images.unsplash.com/photo-1520639888713-7851133b1ed0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9vdHN8ZW58MHx8MHx8&w=1000&q=80" alt="img" />
