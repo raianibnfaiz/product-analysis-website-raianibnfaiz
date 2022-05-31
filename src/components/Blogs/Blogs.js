9fd738e0a2b8c3b1ab4d2132da73bd5113ca9d72
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-05-31 21:46:44 +0600

    ninth commit the answer of third question is added on the blog page.

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -12,6 +12,12 @@ const Blogs = () => {
                 <p className='text-justify mt-5'><span className="text-blue-700 mr-1">Answer:</span>The block element occupies the entire width of its parent div, whereas the inline block element occupies its own width within a div. Inline block components can be stacked one on top of the other depending on their size, however more than one block element can never be stacked on top of each other. An inline element does not start on a new line. A block-level element always starts on a new line, A block-level element always takes up the full width available.</p>
 
             </div>
+            <div className=" second-question p-20">
+                <h3 className='text-center'>What is Semantic Tag?</h3>
+                <p className='text-justify mt-5'><span className="text-blue-700 mr-1">Answer:</span>The nav, article, section etc are also some examples of semantic tag.The tags used in HTML5 are mainly semantic tags. Before HTML5, we would need to create a section, and then declare a lot of div inside that section. As it was difficult to find a simple div so, in HTML5 we can easily divide a HTML body into three semantic tags named header, main, and footer, and it is very easy to find an item of div using a semantic tag.</p>
+
+            </div>
+
         </div>
     );
 };
